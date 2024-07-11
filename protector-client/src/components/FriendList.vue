4b4e334331595727e2784909d887e2394353c2bc
wind <wzsf1810@qq.com>
2024-07-11 23:21:40 +0800

    前端部分

---
+++ protector-client/src/components/FriendList.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import {tableData, click_list} from "../js/friendList"
+
+</script>
+
+<template>
+  <el-row :gutter="20">
+    <el-col :span="1"></el-col>
+      <!-- 功能区 -->
+    <el-col :span="5" style="width: 200px; height: 650px;display: flex; justify-content: center;">
+      <div class="function">
+        <div>
+          <el-image style="margin-top: 10px;width: 45px;height: 45px"
+                    src="https://himg.bdimg.com/sys/portraitn/item/public.1.e4dd1768.84SwiF7xBNN1zG5o1z6wJw"/>
+        </div>
+      </div>
+    </el-col>
+
+    <el-col :span="17">
+      <!--好友列表-->
+      <div class="heard-list">
+        <el-table :data="tableData" class="friend-list"
+                  height="650"
+                  @cell-click="click_list">
+          <el-table-column>
+            <template v-slot="scope">
+              <img :src="scope.row.img_heard" alt="Avatar" style="width: 35px; height: 35px;">
+            </template>
+          </el-table-column>
+          <el-table-column prop="name"/>
+        </el-table>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<style scoped>
+
+</style>
---
+++ protector-client/src/components/Message.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import {user,message_list,getBackgroundColor,input_message} from "@/js/message"
+
+</script>
+
+<template>
+  <el-container>
+    <el-header>
+      <div style="display: flex; align-items: center;">
+        <el-image class="user-header" src="https://sms4j.com/developer/me.jpg"/>
+        <el-text class="mx-1">{{ user.username }}</el-text>
+      </div>
+    </el-header>
+
+    <el-main>
+      <div class="mes-frame">
+        <el-divider />
+        <div class="message" v-for="(item,index) in message_list" :key="index" :style="{backgroundColor: getBackgroundColor(item.source)}">
+          <el-text class="mes-1">{{item.mesUser}}：</el-text>
+          <el-text class="mes-1">{{item.mes}}</el-text>
+        </div>
+      </div>
+    </el-main>
+
+    <el-footer>
+    <!-- 消息发送框-->
+      <el-divider />
+      <el-input style="height: 200px" type="textarea" v-model="input_message" :rows="9" placeholder="请输入消息……" />
+      <el-button style="width: 150px" type="primary" round>发送</el-button>
+    </el-footer>
+  </el-container>
+</template>
+
+<style src="../styles/message.css"></style>
