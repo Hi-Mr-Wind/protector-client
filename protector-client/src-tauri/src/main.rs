4c543b67ac139ae0fa0683bdf5e899a7169972b7
wind <wzsf1810@qq.com>
2024-07-14 00:40:10 +0800

    添加通用模块

--- protector-client/src-tauri/src/main.rs
+++ protector-client/src-tauri/src/main.rs
@@ -1,8 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use protector_lib::entity;
+
+mod db;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
